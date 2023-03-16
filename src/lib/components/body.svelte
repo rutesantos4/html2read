86ad66a5c1664a016a2a75359b1e60225c4d4cd0
RuteSantos <1160663@isep.ipp.pt>
2023-03-16 22:25:55 +0000

    call url inserted by the user

--- src/lib/components/body.svelte
+++ src/lib/components/body.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
 	import { TextInput } from '@components';
+	import { URLQueryImpl, FetchClient, type Read } from '@http';
 
 	let readURL = '';
+	let read: Read;
 
-	function handleSubmitUrl(): void {
+	const query = new URLQueryImpl(new FetchClient());
+
+	async function handleSubmitUrl() {
 		console.info(readURL);
+		const result = await query.getHTML(readURL);
+		if (typeof result === 'number') {
+			return;
+		}
+		read = result;
+		console.log(read.title);
+		console.log(read.description);
+		console.log(read.introduction);
+		console.log(read.summary);
+		console.log(read.keewords);
 	}
 </script>
 
