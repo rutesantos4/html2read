758343b8bf589a6306d4a7df6166482f7b7078f2
RuteSantos <1160663@isep.ipp.pt>
2023-03-17 22:10:30 +0000

    change the title color

--- src/lib/components/heading.svelte
+++ src/lib/components/heading.svelte
@@ -2,6 +2,12 @@
 	export let value: string;
 </script>
 
-<h1 class="text-4xl text-red-500 text-center">
+<h1 id="title" class="text-4xl text-500 text-center">
 	Hello {value}!
 </h1>
+
+<style>
+	#title {
+		color: #9750dd;
+	}
+</style>
