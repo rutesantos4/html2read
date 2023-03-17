d13517baccac5629654ac9259b935436d3bd4bb1
RuteSantos <1160663@isep.ipp.pt>
2023-03-17 22:07:52 +0000

    remove not needed components

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,6 +3,5 @@
 
 export { default as Heading } from './heading.svelte';
 export { default as Body } from './body.svelte';
-export { default as RedBall } from './red_ball.svelte';
 export { default as TextInput } from './text_input.svelte';
-export * from './card';
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script>
-	import { CardNoImage, CardWithImage, Heading, RedBall, Body } from '@components';
+	import { Heading, Body } from '@components';
 	import { LL } from '@i18n';
 </script>
 
 <Heading value="html2read" />
 <Body />
-<div class="flex flex-row justify-evenly">
-	<CardNoImage />
-	<CardWithImage />
-</div>
+
 <div class="flex flex-col justify-center">
-	<RedBall />
 	<p class="text-center">{$LL.greetings()}</p>
 </div>
