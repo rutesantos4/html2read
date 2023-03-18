de8d45f148878ea58a6277398cbd0dd7aaadc573
RuteSantos <1160663@isep.ipp.pt>
2023-03-18 14:25:16 +0000

    add tooltip title in text input

--- src/lib/components/read_form.svelte
+++ src/lib/components/read_form.svelte
@@ -6,7 +6,7 @@
 	export let read: Read;
 
 	async function handleSubmitRead() {
-		read.tags = read.tags + ', ' + read.keewords;
+		read.tags = read.categories + ', ' + read.keewords;
 		read.date = new Date();
 		console.log(read);
 		console.log(read.date);
@@ -20,7 +20,7 @@
 			<TextInput label={$LL.descriptionLabel()} bind:value={read.description} multiline />
 			<TextInput label={$LL.introductionLabel()} bind:value={read.introduction} multiline />
 			<TextInput label={$LL.summaryLabel()} bind:value={read.summary} multiline />
-			<TextInput label={$LL.keewordsLabel()} bind:value={read.keewords} />
+			<TextInput label={$LL.keewordsLabel()} title={$LL.keewordsTootipTitle()} bind:value={read.keewords} />
 			<CheckboxInput label={$LL.draftLabel()} bind:checked={read.draft} />
 			<div class="flex flex-col justify-center mt-8">
 				<button class="button btn btn-secondary" on:click>{$LL.generateButton()}</button>
