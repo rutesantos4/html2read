f8556f7630b39ce841fa126d9fb76b5c54453442
RuteSantos <1160663@isep.ipp.pt>
2023-04-17 20:48:16 +0100

    fix typo in keywords

--- src/lib/components/read_form.svelte
+++ src/lib/components/read_form.svelte
@@ -11,9 +11,9 @@
 <TextInput label={$LL.summaryLabel()} bind:value={read.summary} multiline />
 <TextInput label={$LL.introductionLabel()} bind:value={read.introduction} multiline />
 <TextInput
-	label={$LL.keewordsLabel()}
-	title={$LL.keewordsTootipTitle()}
-	bind:value={read.keewords}
+	label={$LL.keywordsLabel()}
+	title={$LL.keywordsTootipTitle()}
+	bind:value={read.keywords}
 />
 <CheckboxInput label={$LL.draftLabel()} bind:checked={read.draft} />
 <div class="flex flex-col justify-center mt-8">
